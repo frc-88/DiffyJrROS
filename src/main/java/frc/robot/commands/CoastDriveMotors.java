48c7ad5bc832ed684eb47c33b863f2366d3e2ef4
Woz4tetra <woz4tetra@gmail.com>
2022-04-06 10:48:41 -0400

    adding forgotten command

---
+++ src/main/java/frc/robot/commands/CoastDriveMotors.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class CoastDriveMotors extends CommandBase {
+  DriveSubsystem m_drive;
+  /** Creates a new CoastDriveMotors. */
+  public CoastDriveMotors(DriveSubsystem drive) {
+    m_drive = drive;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(m_drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_drive.stop();
+    m_drive.setCoast(true);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_drive.setCoast(false);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  public boolean runsWhenDisabled() {
+      return true;
+  }
+}
