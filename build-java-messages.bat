d9e33570e497d1952729650c427b4309afccd676
Woz4tetra <woz4tetra@gmail.com>
2023-04-18 03:51:12 -0400

    add file

---
+++ build-java-messages.bat
@@ -0,0 +1,11 @@
+@echo on
+
+SET BASE_DIR=%~dp0
+
+SET GEN_DIR=%BASE_DIR%\ros_networktables_bridge_genmsg
+
+call %GEN_DIR%\build-rospy-messages.bat %userprofile%\tj2_ros\src\tj2_interfaces
+set PYTHONPATH=%GEN_DIR%\genmsg\tj2_interfaces:%PYTHONPATH%
+call "%GEN_DIR%\venv\Scripts\activate"
+SET BASE_DIR=%~dp0
+python "%GEN_DIR%\main.py" "-r" "src/main/java" "-m" "frc/robot/ros/messages" "-s" "%BASE_DIR%\source_list.json"
