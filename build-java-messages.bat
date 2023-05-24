d8759b0c7c69774c932b366cc931e52bba299bed
Woz4tetra <woz4tetra@gmail.com>
2023-05-24 13:20:26 -0400

    update scripts

--- build-java-messages.bat
+++ build-java-messages.bat
@@ -2,12 +2,12 @@
 
 SET BASE_DIR=%~dp0
 
-SET GEN_DIR=%BASE_DIR%\ros_networktables_bridge_genmsg
+SET GEN_DIR=%BASE_DIR%ros_networktables_bridge_genmsg
 SET GEN_MSG_ROOT=%GEN_DIR%\genmsg
+SET SOURCES_PATH=%BASE_DIR%source_list.json
 
-call %GEN_DIR%\build-rospy-messages.bat %userprofile%\tj2_ros\src\tj2_interfaces
-set PYTHONPATH=%GEN_MSG_ROOT%\tj2_interfaces:%PYTHONPATH%
+call "%GEN_DIR%\build-rospy-messages.bat"
 call "%GEN_DIR%\venv\Scripts\activate"
-SET BASE_DIR=%~dp0
-python "%GEN_DIR%\clone_repos.py" "%BASE_DIR%\source_list.json" "%GEN_MSG_ROOT%"
-python "%GEN_DIR%\main.py" "-r" "src/main/java" "-m" "frc/robot/ros/messages" "-s" "%BASE_DIR%\source_list.json"
+call "%GEN_MSG_ROOT%\set_build_python_path.bat"
+python "%GEN_DIR%\clone_repos.py" "%SOURCES_PATH%" "%GEN_MSG_ROOT%"
+python "%GEN_DIR%\main.py" "-r" "src/main/java" "-m" "frc/robot/ros/messages" "-s" "%SOURCES_PATH%"
--- build-java-messages.sh
+++ build-java-messages.sh
@@ -2,9 +2,9 @@ BASE_DIR=$(realpath "$(dirname $0)")
 
 GEN_DIR=${BASE_DIR}/ros_networktables_bridge_genmsg
 GEN_MSG_ROOT=${GEN_DIR}/genmsg
+SOURCES_PATH=${BASE_DIR}/source_list.json
 
-${GEN_DIR}/build-rospy-messages.sh ~/tj2_ros/src/tj2_interfaces
-export PYTHONPATH=${GEN_MSG_ROOT}/tj2_interfaces:$PYTHONPATH
+${GEN_DIR}/build-rospy-messages.sh ${SOURCES_PATH}
 source ${GEN_DIR}/venv/bin/activate
-python3 ${GEN_DIR}/clone_repos.py ${BASE_DIR}/source_list.json ${GEN_MSG_ROOT}
-python3 ${GEN_DIR}/main.py -r 'src/main/java' -m 'frc/robot/ros/messages' -s ${BASE_DIR}/source_list.json
+source ${GEN_MSG_ROOT}/set_build_python_path.sh
+python3 ${GEN_DIR}/main.py -r 'src/main/java' -m 'frc/robot/ros/messages' -s ${SOURCES_PATH}
