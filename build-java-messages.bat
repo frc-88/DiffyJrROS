942784d17b4763c1b28797afcfddd7460394ca96
Woz4tetra <woz4tetra@gmail.com>
2023-05-24 22:19:13 -0400

    windows fixes

--- build-java-messages.bat
+++ build-java-messages.bat
@@ -6,8 +6,7 @@ SET GEN_DIR=%BASE_DIR%ros_networktables_bridge_genmsg
 SET GEN_MSG_ROOT=%GEN_DIR%\genmsg
 SET SOURCES_PATH=%BASE_DIR%source_list.json
 
-call "%GEN_DIR%\build-rospy-messages.bat"
+call "%GEN_DIR%\build-rospy-messages.bat" "%SOURCES_PATH%"
 call "%GEN_DIR%\venv\Scripts\activate"
 call "%GEN_MSG_ROOT%\set_build_python_path.bat"
-python "%GEN_DIR%\clone_repos.py" "%SOURCES_PATH%" "%GEN_MSG_ROOT%"
 python "%GEN_DIR%\main.py" "-r" "src/main/java" "-m" "frc/robot/ros/messages" "-s" "%SOURCES_PATH%"
