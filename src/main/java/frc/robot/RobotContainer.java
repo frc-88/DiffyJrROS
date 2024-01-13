31bf3917cf0d5da31bbd1d0f5d3490511a807a06
Woz4tetra <woz4tetra@gmail.com>
2024-01-13 13:23:30 -0500

    remove unused

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -22,7 +22,6 @@ import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
